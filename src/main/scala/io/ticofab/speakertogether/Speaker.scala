011893856085d81a426e1bdcc43604df81e6b46e
Fabio Tiriticco <fabio.europe@gmail.com>
2021-03-02 11:28:55 +0100

    less hardcoding, still hardcoded

--- src/main/scala/io/ticofab/speakertogether/Speaker.scala
+++ src/main/scala/io/ticofab/speakertogether/Speaker.scala
@@ -16,18 +16,19 @@ object SpeakerEnriched {
   val fromSpeakerCsvEntry: SpeakerCsvEntry => SpeakerEnriched = {
     case SpeakerCsvEntry(d, c, n) =>
       val normalizedName = StringUtils.normalize(n).toLowerCase
-      SpeakerEnriched(d, c, n, normalizedName, twitterUrls.get(normalizedName))
+      val maybeTwitterUrl = twitterHandles.get(normalizedName).map("https://twitter.com/" + _)
+      SpeakerEnriched(d, c, n, normalizedName, maybeTwitterUrl)
   }
 
   // hardcoded stuff FTW 😅
-  val twitterUrls: Map[String, String] = Map(
-    "adam sandor" -> "https://twitter.com/adamsand0r",
-    "alessandro vozza" -> "https://twitter.com/bongo",
-    "wiem zine elabidine" -> "https://twitter.com/WiemZin",
-    "nicolas rinaudo" -> "https://twitter.com/NicolasRinaudo",
-    "pierangelo cecchetto" -> "https://twitter.com/pierangelocecc",
-    "paulo lopes" -> "https://twitter.com/pml0pes",
-    "jakub kozowski" -> "https://twitter.com/kubukoz"
+  val twitterHandles: Map[String, String] = Map(
+    "adam sandor" -> "adamsand0r",
+    "alessandro vozza" -> "bongo",
+    "wiem zine elabidine" -> "WiemZin",
+    "nicolas rinaudo" -> "NicolasRinaudo",
+    "pierangelo cecchetto" -> "pierangelocecc",
+    "paulo lopes" -> "pml0pes",
+    "jakub kozowski" -> "kubukoz"
   )
 
 }
